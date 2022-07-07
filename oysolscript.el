765f293b1b2b25a63659520fb0548d366e2a1426
oysol <cordon.impetus_0m@icloud.com>
2022-07-07 19:02:27 +0900

    Initial commit

---
+++ oysolscript.el
@@ -0,0 +1,337 @@
+(defun oysolscript ()
+(interactive)
+(let ((case-fold-search nil))
+
+  (goto-char (point-min))
+  (while (search-forward "..." nil t)
+    (replace-match "…"))
+  (goto-char (point-min))
+  (while (search-forward "KH" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "kh" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "K" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "k" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "NG" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "ng" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "N" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "n" nil t)
+    (replace-match ""))
+
+
+  (goto-char (point-min))
+  (while (search-forward "TH" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "th" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "T" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "t" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "L" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "l" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "M" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "m" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "PH" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "ph" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "P" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "p" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "S" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "s" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "CH" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "ch" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "C" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "c" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "H" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "h" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "YAY" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "YAy" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "yay" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "YA" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "ya" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "WAY" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Way" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "way" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "WA" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Wa" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "wa" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "AY" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Ay" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "ay" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "A" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "a" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "YEY" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "YEy" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "yey" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "YE" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "ye" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "EY" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Ey" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "ey" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "WEY" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Wey" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "wey" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "WE" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "We" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "we" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "E" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "e" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "OY" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Oy" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "oy" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "YO" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "yo" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "O" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "o" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "WU" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "wu" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "YU" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "yu" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "UY" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Uy" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "uy" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "U" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "u" nil t)
+    (replace-match ""))
+    
+  (goto-char (point-min))
+  (while (search-forward "WI" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "Wi" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "wi" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "I" nil t)
+    (replace-match ""))
+
+  (goto-char (point-min))
+  (while (search-forward "i" nil t)
+    (replace-match ""))
+
+
+  ;; repeat for other string pairs
+
+  ))
+(global-set-key (kbd "C-x y") 'oysolscript)
